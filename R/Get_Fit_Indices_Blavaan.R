de588a9cde756af06bcab0820806cafa024d0bbc
jventural <jventuraleon@gmail.com>
2024-01-29 17:21:23 -0500

    update 2024

--- R/Get_Fit_Indices_Blavaan.R
+++ R/Get_Fit_Indices_Blavaan.R
@@ -1,10 +1,18 @@
 Get_Fit_Indices_Blavaan <- function(model, credMass = 0.95) {
+  library(tidyverse)
+  if (!require(blavaan)) {
+    stop("El paquete 'blavaan' es necesario para esta función.")
+  }
+
   # Calcular medias
   chisq_mean <- mean(model@details$chisq)
   pD_mean <- mean(model@details$pD)
   df_mean <- mean(model@details$df)
   brmsea_mean <- mean(model@indices$BRMSEA)
   bcfi_mean <- mean(model@indices$BCFI)
+
+  # Extraer ppp como el tercer elemento de fitMeasures
+  ppp <- (fitMeasures(fit1) %>% as_tibble() %>% slice(3) %>% pull())
 
   # Calcular HDI para BRMSEA
   hdi_brmsea <- HDInterval::hdi(model@indices$BRMSEA, credMass = credMass)
@@ -22,11 +30,12 @@ Get_Fit_Indices_Blavaan <- function(model, credMass = 0.95) {
     chisq_mean,
     pD_mean,
     df_mean,
+    ppp, # Añadir ppp aquí
     brmsea_mean,
     brmsea_hdi,
     bcfi_mean,
     bcfi_hdi
-  ), c("chisq", "pD", "df", "BRMSEA", ci_col_name, "BCFI", ci_col_name))
+  ), c("chisq", "pD", "df", "ppp", "BRMSEA", ci_col_name, "BCFI", ci_col_name))
 
   return(summary_df)
 }
