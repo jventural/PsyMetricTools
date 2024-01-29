5b026666b17da517d850f39fc9e802893e2da9b0
jventural <jventuraleon@gmail.com>
2024-01-29 11:32:34 -0500

    update 2024

--- R/calculate_mcmc_standard_error.R
+++ R/calculate_mcmc_standard_error.R
@@ -1,29 +1,44 @@
-calculate_mcmc_standard_error <- function(stpost1) {
-  if (!is.matrix(stpost1) && !is.data.frame(stpost1)) {
-    stop("stpost1 debe ser una matriz o un data frame.")
-  }
+calculate_mcmc_standard_error <- function(fit1) {
+  # Asegúrate de que las dependencias están cargadas
+  if (!require(mcmcse)) install.packages("mcmcse")
+  library(mcmcse)
 
-  # Inicializar el vector de error estándar
-  se1 <- numeric(ncol(stpost1))
-  for (i in 1:ncol(stpost1)) {
-    mcse_result <- mcmcse::mcse(stpost1[, i])
-    if (is.list(mcse_result)) {
-      se1[i] <- mcse_result[[2]] # Asumiendo que el segundo elemento es el SE
-    } else {
-      se1[i] <- mcse_result
+  # Aplica la función 'standardizedPosterior' al modelo 'fit1'
+  stpost1 <- standardizedPosterior(fit1)
+  stpost1 <- as.data.frame(stpost1)
+
+  # Define la función 'calculate_mcmc_standard_error' internamente
+  calculate_mcmc_standard_error <- function(stpost1) {
+
+    if (!is.matrix(stpost1) && !is.data.frame(stpost1)) {
+      stop("stpost1 debe ser una matriz o un data frame.")
     }
+
+    # Inicializar el vector de error estándar
+    se1 <- numeric(ncol(stpost1))
+    for (i in 1:ncol(stpost1)) {
+      mcse_result <- mcmcse::mcse(stpost1[, i])
+      if (is.list(mcse_result)) {
+        se1[i] <- mcse_result[[2]] # Asumiendo que el segundo elemento es el SE
+      } else {
+        se1[i] <- mcse_result
+      }
+    }
+
+    # Verificar la condición del error estándar
+    checkvar1 <- se1 < 0.05 * apply(stpost1, 2, sd)
+
+    # Formatear los valores mínimos y máximos para evitar la notación científica
+    min_se <- format(min(se1[se1 > 0]), scientific = FALSE)
+    max_se <- format(max(se1), scientific = FALSE)
+
+    # Formatear se1 para evitar la notación científica
+    se1_formatted <- format(se1, scientific = FALSE)
+
+    # Devolver una lista con los resultados y el vector completo de se1 formateado
+    return(list(min_se = min_se, max_se = max_se, checkvar1 = checkvar1, se1 = se1_formatted))
   }
 
-  # Verificar la condición del error estándar
-  checkvar1 <- se1 < 0.05 * apply(stpost1, 2, sd)
-
-  # Formatear los valores mínimos y máximos para evitar la notación científica
-  min_se <- format(min(se1[se1 > 0]), scientific = FALSE)
-  max_se <- format(max(se1), scientific = FALSE)
-
-  # Formatear se1 para evitar la notación científica
-  se1_formatted <- format(se1, scientific = FALSE)
-
-  # Devolver una lista con los resultados y el vector completo de se1 formateado
-  return(list(min_se = min_se, max_se = max_se, checkvar1 = checkvar1, se1 = se1_formatted))
+  # Llama a la función 'calculate_mcmc_standard_error' con el resultado de 'standardizedPosterior'
+  return(calculate_mcmc_standard_error(stpost1))
 }
