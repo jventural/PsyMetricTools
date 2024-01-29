b74b3358b6e034e0ee3d22e6f8202940ebe45471
jventural <jventuraleon@gmail.com>
2024-01-29 17:30:44 -0500

    update 2024

---
+++ R/Preliminar_table.R
@@ -0,0 +1,12 @@
+Preliminar_table <- function(data) {
+  library(dplyr)
+  library(psych)
+  data %>%
+    psych::response.frequencies() %>%
+    as_tibble() %>%
+    select(-miss) %>%
+    mutate(Items = c(1:3)) %>%
+    relocate(Items) %>%
+    mutate(across(`1`:`5`, ~ .x * 100)) %>%
+    mutate(across(where(is.numeric), round, 2))
+}
