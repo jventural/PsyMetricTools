96e92f22fc0e698b7dbcb451dcf4866dd199d869
jventural <jventuraleon@gmail.com>
2024-02-02 16:28:22 -0500

    update febrero 2024

--- R/Preliminar_table.R
+++ R/Preliminar_table.R
@@ -4,7 +4,7 @@ Preliminar_table <- function(data) {
   data %>%
     psych::response.frequencies() %>%
     as_tibble() %>%
-    select(-miss) %>%
+    dplyr::select(-miss) %>%
     dplyr::mutate(Items = c(1:3)) %>%
     relocate(Items) %>%
     dplyr::mutate(across(`1`:`5`, ~ .x * 100)) %>%
