3e96d11c21d7b8278b2f92cb1914f296de6312fc
jventural <jventuraleon@gmail.com>
2024-02-09 18:22:46 -0500

    update febrero 2024

--- R/generate_modelos.R
+++ R/generate_modelos.R
@@ -1,6 +1,11 @@
-generate_modelos <- function(n_factors, n_items, name_items, exclude_items = NULL) {
-  generate_models <- function(n_factors, n_items, name_items, exclude_items) {
-    var_names <- paste0(name_items, 1:n_items)
+generate_modelos <- function(n_factors, specific_items = NULL, name_items = NULL, n_items = NULL, exclude_items = NULL) {
+  generate_models <- function(n_factors, specific_items, name_items, n_items, exclude_items) {
+    # Si se proporcionan specific_items, se utilizan directamente. De lo contrario, se generan los nombres de los ítems.
+    if (!is.null(specific_items)) {
+      var_names <- specific_items
+    } else {
+      var_names <- paste0(name_items, 1:n_items)
+    }
 
     if (!is.null(exclude_items)) {
       var_names <- setdiff(var_names, exclude_items)
@@ -19,6 +24,6 @@ generate_modelos <- function(n_factors, n_items, name_items, exclude_items = NUL
     return(models)
   }
 
-  models <- generate_models(n_factors, n_items, name_items, exclude_items)
+  models <- generate_models(n_factors, specific_items, name_items, n_items, exclude_items)
   return(models)
 }
