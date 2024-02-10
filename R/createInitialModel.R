8865aca04adec2be74e5e068b664d00ca69bde92
jventural <jventuraleon@gmail.com>
2024-02-10 16:33:53 -0500

    Interactive

---
+++ R/createInitialModel.R
@@ -0,0 +1,10 @@
+createInitialModel <- function(n_factors, n_items, name_items, exclude_items = NULL) {
+  initialModel <- list(
+    n_factors = n_factors,
+    n_items = n_items,
+    name_items = name_items,
+    exclude_items = exclude_items
+  )
+
+  return(initialModel)
+}
