db56c5f1d38afcf0b9471f99be48b2dd7b7e1b2b
jventural <jventuraleon@gmail.com>
2024-02-12 07:49:15 -0500

    EFA Algorithm

---
+++ R/extract_bondad_boot.R
@@ -0,0 +1,31 @@
+extract_bondad_bootnet <- function(resultados_bootstrap) {
+  # Cargar las librerías necesarias
+  require(purrr)
+  require(dplyr)
+  require(tibble)
+
+  # Función interna para convertir Bondades a list y añadir el número de muestra
+  convertBondadesToList <- function(bondadesList, muestra) {
+    bondadesList %>%
+      map_dfr(~as_tibble(.), .id = "ID") %>%
+      mutate(Muestra = muestra)
+  }
+
+  # Función interna para aplicar convertBondadesToList a cada elemento de resultados_bootstrap
+  applyConvertBondadesToList <- function(bootstrapResults) {
+    allBondadesDf <- map_dfr(seq_along(bootstrapResults$Results), function(muestra) {
+      convertBondadesToList(bootstrapResults$Results[[muestra]]$CombinedResults$Bondades, muestra)
+    }, .id = "MuestraID")
+
+    # Opcional: Convertir MuestraID a un número si es necesario
+    allBondadesDf <- allBondadesDf %>%
+      mutate(MuestraID = as.integer(MuestraID))
+
+    return(allBondadesDf)
+  }
+
+  # Aplicar la función al conjunto de resultados de bootstrap
+  Bondades_ajuste <- applyConvertBondadesToList(resultados_bootstrap)
+
+  return(Bondades_ajuste)
+}
