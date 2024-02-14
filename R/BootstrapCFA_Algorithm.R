6df1e2e92d3747f133efbc4ca295d63f84e786fc
jventural <jventuraleon@gmail.com>
2024-02-13 20:42:45 -0500

    update CFA algorithm

--- R/BootstrapCFA_Algorithm.R
+++ R/BootstrapCFA_Algorithm.R
@@ -10,24 +10,27 @@ BootstrapCFA_Algorithm <-  function(originalData, initialModel, n_samples = 10, 
     })
     return(bootstrapped_samples)
   }
+
   bootstrapped_samples <- generateBootstrapSamples(originalData, n_samples, seed)
 
   cfaResults <- list()
 
-  # Iniciar la barra de progreso
-  pb <- txtProgressBar(min = 0, max = length(bootstrapped_samples), style = 3)
-
   for (i in 1:length(bootstrapped_samples)) {
+    # Mensaje que indica en qué iteración de muestra de bootstrap se encuentra
+    cat("Iniciando la iteración", i, "de", length(bootstrapped_samples), "\n")
     bootstrapData <- bootstrapped_samples[[i]]
+
+    # Iniciar la barra de progreso para la iteración actual
+    pb <- txtProgressBar(min = 0, max = MAX_ITERATIONS, style = 3)
+
     cfaResult <- iterativeModelCFA(data = bootstrapData, initialModel = initialModel, MAX_ITERATIONS = MAX_ITERATIONS, MI_THRESHOLD = MI_THRESHOLD)
+
+    # Cerrar la barra de progreso de la iteración actual
+    close(pb)
+
+    cat("Iteración", i, "completada\n\n")  # Mensaje al completar la iteración
     cfaResults[[i]] <- cfaResult
-
-    # Actualizar la barra de progreso
-    setTxtProgressBar(pb, i)
   }
-
-  # Cerrar la barra de progreso
-  close(pb)
 
   return(cfaResults)
 }
