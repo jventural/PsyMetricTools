52b0863b0b64b170a5e39859a048e4c12febfc14
jventural <jventuraleon@gmail.com>
2024-03-04 13:37:35 -0500

    standarized_solutions

--- R/Standardized_solutions.R
+++ R/Standardized_solutions.R
@@ -7,33 +7,45 @@ Standardized_solutions <- function(specification, name_items, apply_threshold = 
     library(package, character.only = TRUE)
   }
 
-  # Instalar y cargar la librería requerida
+  # Instalar y cargar las librerías requeridas
   install_and_load("tidyr")
   install_and_load("dplyr")
   install_and_load("stringr")
 
+  # Procesamiento de los datos
   result <- standardizedsolution(specification) %>%
     filter(op == "=~") %>%
-    mutate(item  = str_remove(rhs, name_items) %>% as.double(),
+    mutate(item = str_remove(rhs, name_items) %>% as.double(),
            factor = str_remove(lhs, "f")) %>%
     dplyr::select(lhs, rhs, est.std) %>%
-    pivot_wider(
-      names_from = lhs,
-      values_from = c(est.std))
+    pivot_wider(names_from = lhs, values_from = c(est.std))
 
-  num_factors <- ncol(result) - 1
-
+  # Aplicar el umbral
   if (apply_threshold) {
     result <- result %>%
-      mutate(across(paste0("f", 1:num_factors), ~ case_when(
-        . <= 0.30 ~ 0, TRUE ~ .)))
+      mutate(across(starts_with("f"), ~ ifelse(abs(.) > 0.30, ., 0)))
   }
 
-  factor_names <- paste0("f", 1:num_factors)
+  # Identificar el factor principal para cada ítem
   result <- result %>%
-    arrange_at(vars(factor_names), .funs = list(desc)) %>%
+    rowwise() %>%
+    mutate(max_factor = list(which(abs(c_across(starts_with("f"))) == max(abs(c_across(starts_with("f"))), na.rm = TRUE), arr.ind = TRUE))) %>%
+    ungroup()
+
+  # Extraer el índice del factor principal y su valor
+  result$max_factor_index <- sapply(result$max_factor, function(x) if (length(x) > 0) x[1] else NA)
+  result$max_factor_value <- sapply(result$max_factor, function(x) if (length(x) > 0) x[2] else NA)
+
+  # Ordenar por el índice del factor principal y luego por el valor absoluto de la carga
+  result <- result %>%
+    arrange(max_factor_index, desc(abs(max_factor_value))) %>%
+    select(-max_factor, -max_factor_index, -max_factor_value) # Limpiar las columnas temporales
+
+  # Renombrar para la salida final
+  result <- result %>%
     rename(Items = rhs)
 
   return(result)
 }
+
 
