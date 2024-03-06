f945a53a264f804beb09dbc5962ab4545011edf4
jventural <jventuraleon@gmail.com>
2024-03-06 17:16:41 -0500

    iterative_EFA

--- R/specification_models.R
+++ R/specification_models.R
@@ -1,4 +1,4 @@
-specification_models <- function(modelos, data, estimator) {
+specification_models <- function(modelos, data, estimator, rotation = "oblimin", verbose = FALSE) {
   # Función para instalar y cargar librerías
   install_and_load <- function(package) {
     if (!requireNamespace(package, quietly = TRUE)) {
@@ -10,18 +10,22 @@ specification_models <- function(modelos, data, estimator) {
   # Instalar y cargar la librería requerida
   install_and_load("lavaan")
 
-  AD <- list()
+  specification <- list()
 
   for (i in 1:length(modelos)) {
     fit.original = cfa(paste0(modelos[i]),
                        data = data,
                        estimator = estimator,
-                       rotation = "oblimin",
+                       rotation = rotation,
                        mimic = "Mplus",
-                       ordered = TRUE)
-    AD[[i]] = fit.original
-    print(fit.original)
+                       ordered = TRUE,
+                       verbose = verbose)  # Usar el argumento verbose aquí
+    specification[[i]] = fit.original
+
+    if (verbose) {  # Si verbose es TRUE, imprimir el modelo
+      print(fit.original)
+    }
   }
 
-  return(AD)
+  return(specification)
 }
