208eefe8defbfa028f9597de02fde88621ddeeaf
jventural <jventuraleon@gmail.com>
2024-03-28 23:49:58 -0500

    invertir_items

--- R/invertir_items.R
+++ R/invertir_items.R
@@ -1,12 +1,14 @@
-invertir_items <- function(df, items) {
+invertir_items <- function(df, columns, num_respuestas, comienza_con_cero = TRUE) {
   df %>%
-    mutate(across(all_of(items), ~ {
-      val_uniq = unique(.)
-      if(0 %in% val_uniq) {
-        return(3 - .)
+    mutate(across(all_of(columns), function(x) {
+      if (comienza_con_cero) {
+        # Si las respuestas comienzan con 0, el máximo valor es num_respuestas - 1
+        max_val = num_respuestas - 1
+        return(max_val + 1 - x)
       } else {
-        max_val = max(val_uniq)
-        return(max_val + 1 - .)
+        # Si las respuestas comienzan con 1, el máximo valor es num_respuestas
+        max_val = num_respuestas
+        return(max_val + 1 - x)
       }
     }))
 }
