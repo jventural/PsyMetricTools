3ad24bc1b6b0ca93e8ed4bf24fb2ef1a3b74ef0c
jventural <jventuraleon@gmail.com>
2024-05-08 21:14:50 -0500

    Plot_Likert

--- R/Plot_Likert.R
+++ R/Plot_Likert.R
@@ -1,54 +1,46 @@
 Plot_Likert <- function(Data, name_items, ranges, exclude = NULL) {
-  library(tidyverse)
-  # Función para instalar y cargar librerías
-  install_and_load <- function(package) {
-    if (!requireNamespace(package, quietly = TRUE)) {
-      install.packages(package)
-    }
-    library(package, character.only = TRUE)
+  # Carga los paquetes necesarios
+  if (!requireNamespace("ggplot2", quietly = TRUE)) {
+    install.packages("ggplot2")
   }
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    install.packages("dplyr")
+  }
+  if (!requireNamespace("tidyr", quietly = TRUE)) {
+    install.packages("tidyr")
+  }
+  library(ggplot2)
+  library(dplyr)
+  library(tidyr)
 
-  # Instalar y cargar las librerías requeridas
-  install_and_load("tidyverse")
-  install_and_load("ggplot2")
-
-  # Crear los nombres de las columnas
+  # Construir nombres de columnas basados en los rangos especificados
   cols <- paste0(name_items, ranges)
 
-  # Remover los ítems a excluir
+  # Excluir columnas si se especifica
   if (!is.null(exclude)) {
     exclude_cols <- paste0(name_items, exclude)
     cols <- setdiff(cols, exclude_cols)
   }
 
-  # Comprobar que las columnas existen en el dataframe
+  # Verificar que todas las columnas especificadas existan en el dataframe
   if (!all(cols %in% names(Data))) {
     stop("Algunas columnas especificadas no existen en el dataframe.")
   }
 
-  # Convertir a formato largo
+  # Transformar datos para el gráfico
   df_pivot <- Data %>%
-    pivot_longer(
-      cols = all_of(cols),
-      names_to = "Variables",
-      values_to = "Var_Ptjes"
-    ) %>%
-    mutate(
-      Variables = factor(
-        Variables,
-        levels = Data %>% dplyr::select(all_of(cols)) %>% names()
-      )
-    ) %>%
-    dplyr::select(Variables, Var_Ptjes)
+    pivot_longer(cols = all_of(cols), names_to = "Variables", values_to = "Var_Ptjes") %>%
+    mutate(Variables = factor(Variables, levels = select(Data, all_of(cols)) %>% names())) %>%
+    select(Variables, Var_Ptjes)
 
-  # Crear el gráfico
-  ggplot(df_pivot, aes(x= as.factor(Var_Ptjes))) +
-    geom_bar(fill = "#CCD1D1", colour="black") +
-    geom_text(stat='count', aes(label=scales::percent(..count../nrow(Data),1)), vjust=-0.1,size = 3)+
-    scale_y_continuous(limits=c(0,nrow(Data)))+
-    facet_wrap(vars(Variables), scale = "free_x") +
-    theme_bw()+
-    labs(x = "Tasas de respuesta")+
-    labs(y = "")+
+  # Generar el gráfico
+  ggplot(df_pivot, aes(x = as.factor(Var_Ptjes))) +
+    geom_bar(fill = "#CCD1D1", colour = "black") +
+    geom_text(stat = "count", aes(label = scales::percent(..count../nrow(Data), accuracy = 0.01)),
+              vjust = -0.1, size = 3) +
+    scale_y_continuous(limits = c(0, nrow(Data))) +
+    facet_wrap(~ Variables, scale = "free_x") +
+    theme_bw() +
+    labs(x = "Tasas de respuesta", y = "") +
     theme(axis.text.y = element_blank())
 }
