ea63de9bdf98f53c6c04b17858d6d2f8a49b6d4e
jventural <jventuraleon@gmail.com>
2024-08-25 21:38:53 -0500

    update

--- R/plot_cfa_stability.R
+++ R/plot_cfa_stability.R
@@ -1,20 +1,20 @@
 plot_cfa_stability <- function(resultados,
-                           y_min_cfi = 0.9, y_max_cfi = 1,
-                           y_min_tli  = 0.9, y_max_tli = 1,
-                           y_min_rmsea = 0, y_max_rmsea = 0.15,
-                           y_min_srmr = 0, y_max_srmr = 0.15,
-                           y_min_crmr = 0, y_max_crmr = 0.15,
-                           y_min_reliability = 0.50, y_max_reliability = 1,
-                           y_breaks = 0.01,
-                           y_breaks_reliability = 0.05,
-                           hline_color = "red",
-                           xlab_size = 12,
-                           ylab_size = 12,
-                           label_size = 10) {
+                                y_min_cfi = 0.9, y_max_cfi = 1,
+                                y_min_tli = 0.9, y_max_tli = 1,  # Agregado para TLI
+                                y_min_rmsea = 0, y_max_rmsea = 0.15,
+                                y_min_srmr = 0, y_max_srmr = 0.15,
+                                y_min_crmr = 0, y_max_crmr = 0.15,
+                                y_min_reliability = 0.50, y_max_reliability = 1,
+                                y_breaks = 0.01,
+                                y_breaks_reliability = 0.05,
+                                hline_color = "red",
+                                xlab_size = 12,
+                                ylab_size = 12) {
   library(tidyverse)
   library(ggplot2)
   library(ggpubr)
 
+  # Cálculo y gráfico de CFI
   CFI_stability <- resultados %>%
     group_by(Porcentaje) %>%
     summarise(CFI = mean(cfi.scaled)) %>%
@@ -31,6 +31,7 @@ plot_cfa_stability <- function(resultados,
     theme(axis.text.x = element_text(size = xlab_size),
           axis.text.y = element_text(size = ylab_size))
 
+  # Cálculo y gráfico de TLI
   TLI_stability <- resultados %>%
     group_by(Porcentaje) %>%
     summarise(TLI = mean(tli.scaled)) %>%
@@ -47,9 +48,7 @@ plot_cfa_stability <- function(resultados,
     theme(axis.text.x = element_text(size = xlab_size),
           axis.text.y = element_text(size = ylab_size))
 
-
-
-
+  # Cálculo y gráfico de RMSEA
   RMSEA_stability <- resultados %>%
     group_by(Porcentaje) %>%
     summarise(RMSEA = mean(rmsea.scaled)) %>%
@@ -66,6 +65,7 @@ plot_cfa_stability <- function(resultados,
     theme(axis.text.x = element_text(size = xlab_size),
           axis.text.y = element_text(size = ylab_size))
 
+  # Cálculo y gráfico de SRMR
   SRMR_stability <- resultados %>%
     group_by(Porcentaje) %>%
     summarise(SRMR = mean(srmr)) %>%
@@ -82,6 +82,7 @@ plot_cfa_stability <- function(resultados,
     theme(axis.text.x = element_text(size = xlab_size),
           axis.text.y = element_text(size = ylab_size))
 
+  # Cálculo y gráfico de CRMR
   CRMR_stability <- resultados %>%
     group_by(Porcentaje) %>%
     summarise(CRMR = mean(crmr)) %>%
@@ -98,6 +99,7 @@ plot_cfa_stability <- function(resultados,
     theme(axis.text.x = element_text(size = xlab_size),
           axis.text.y = element_text(size = ylab_size))
 
+  # Cálculo y gráfico de fiabilidad (reliability)
   reliability_stability <- resultados %>%
     group_by(Porcentaje) %>%
     summarise(reliability = mean(F1)) %>%
@@ -114,9 +116,9 @@ plot_cfa_stability <- function(resultados,
     theme(axis.text.x = element_text(size = xlab_size),
           axis.text.y = element_text(size = ylab_size))
 
+  # Combinar gráficos
   figure3 <- suppressWarnings(ggarrange(p4, p5, p6, p7, p8, p9,
                                         labels = c("A", "B", "C", "D", "E", "F"),
-                                        label_size = label_size,
                                         ncol = 3, nrow = 2))
 
   return(figure3)
