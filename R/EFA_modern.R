1e994f5e5d08574e1b64f80d819c024cec840a55
jventural <jventuraleon@gmail.com>
2024-08-26 22:02:58 -0500

    efa_modern

---
+++ R/EFA_modern.R
@@ -0,0 +1,26 @@
+EFA_modern <- function(n_factors, n_items, name_items, data, apply_threshold, estimator = "WLSMV", exclude_items = NULL) {
+  library(lavaan)
+  library(dplyr)
+  # Generar modelo para lavaan exploratorio
+  modelos <- generate_modelos(n_factors = n_factors, n_items = n_items, name_items = name_items, exclude_items = exclude_items)
+
+  # Especificación para lavaan exploratorio
+  Specifications <- specification_models(modelos, data = data, estimator = estimator)
+
+  # Bondades de ajuste para lavaan exploratorio
+  Bondades_Original <- extract_fit_measures(Specifications)
+
+  # Matriz patron de lavaan exploratorio
+  result_df <- Standardized_solutions(Specifications[[n_factors]], name_items = name_items, apply_threshold = apply_threshold)
+
+  # Correlación entre factores
+  InterFactor <- lavaan::inspect(Specifications[[n_factors]], what = "std")$psi
+
+  # Retornar todos los resultados
+  return(list(
+    Bondades_Original = Bondades_Original,
+    Specifications = Specifications,
+    InterFactor = InterFactor,
+    result_df = result_df
+  ))
+}
