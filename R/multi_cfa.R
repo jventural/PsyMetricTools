5f56597d3ed86452936db6d205d03f3eee5d2aa1
jventural <jventuraleon@gmail.com>
2024-09-02 21:56:24 -0500

    multi_cfa

--- R/multi_cfa.R
+++ R/multi_cfa.R
@@ -1,23 +1,44 @@
 multi_cfa <- function(modelos, data, estimator, ordered = TRUE, orthogonal_indices = NULL) {
   ajustar_modelo <- function(modelo, index, data, estimator, ordered, orthogonal_indices) {
-    # Verificar si el modelo actual necesita el argumento orthogonal = TRUE
-    if (!is.null(orthogonal_indices) && index %in% orthogonal_indices) {
-      fit <- cfa(modelo, data = data, estimator = estimator, mimic = "Mplus", ordered = ordered, orthogonal = TRUE)
-    } else {
-      fit <- cfa(modelo, data = data, estimator = estimator, mimic = "Mplus", ordered = ordered)
-    }
+    resultado <- tryCatch({
+      # Verificar si el modelo actual necesita el argumento orthogonal = TRUE
+      if (!is.null(orthogonal_indices) && index %in% orthogonal_indices) {
+        fit <- cfa(modelo, data = data, estimator = estimator, mimic = "Mplus", ordered = ordered, orthogonal = TRUE)
+      } else {
+        fit <- cfa(modelo, data = data, estimator = estimator, mimic = "Mplus", ordered = ordered)
+      }
 
-    # Calcular medidas de bondad de ajuste
-    bondad_ajuste <- round(fitMeasures(fit, c("chisq.scaled", "df.scaled", "srmr", "wrmr", "cfi.scaled", "tli.scaled", "rmsea.scaled", "crmr")), 3)
-    indices_modificacion <- modificationIndices(fit, sort = TRUE) %>% dplyr::filter(mi > 10) %>% mutate(mi = round(mi, 2))
-    correlaciones_factores <- round(as.data.frame(inspect(fit, what = "std")$psi), 2)
-    fiabilidad <- round(compRelSEM(fit, tau.eq = F, ord.scale = T), 2)
+      # Calcular medidas de bondad de ajuste
+      bondad_ajuste <- round(fitMeasures(fit, c("chisq.scaled", "df.scaled", "srmr", "wrmr", "cfi.scaled", "tli.scaled", "rmsea.scaled", "crmr")), 3)
+      indices_modificacion <- modificationIndices(fit, sort = TRUE) %>% dplyr::filter(mi > 10) %>% mutate(mi = round(mi, 2))
+      correlaciones_factores <- round(as.data.frame(inspect(fit, what = "std")$psi), 2)
+      fiabilidad <- round(compRelSEM(fit, tau.eq = F, ord.scale = T), 2)
 
-    return(list(fit = fit,
-                bondad_ajuste = bondad_ajuste,
-                indices_modificacion = indices_modificacion,
-                correlaciones_factores = correlaciones_factores,
-                fiabilidad = fiabilidad))
+      return(list(fit = fit,
+                  bondad_ajuste = bondad_ajuste,
+                  indices_modificacion = indices_modificacion,
+                  correlaciones_factores = correlaciones_factores,
+                  fiabilidad = fiabilidad))
+    }, error = function(e) {
+      # Si ocurre un error, retornamos una lista con NA y un mensaje de error
+      bondad_ajuste <- data.frame(
+        chisq.scaled = NA,
+        df.scaled = NA,
+        srmr = NA,
+        wrmr = NA,
+        cfi.scaled = NA,
+        tli.scaled = NA,
+        rmsea.scaled = NA,
+        crmr = NA,
+        mensaje = "model does not converge"
+      )
+      return(list(fit = NULL,
+                  bondad_ajuste = bondad_ajuste,
+                  indices_modificacion = NA,
+                  correlaciones_factores = NA,
+                  fiabilidad = NA))
+    })
+    return(resultado)
   }
 
   # Aplicar la función ajustar_modelo a cada modelo
